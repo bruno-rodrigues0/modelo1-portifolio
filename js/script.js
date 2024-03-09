a785b1bb54af9e2e760f8ec71d8757c548a91d5d
Fizene <119943937+bruno-rodrigues0@users.noreply.github.com>
2024-03-09 20:56:01 -0300

    PLUS ULTRA

--- js/script.js
+++ js/script.js
@@ -1,12 +1,15 @@
 var hover = document.querySelectorAll(".hoverlink");
 var bg = document.querySelectorAll(".bg-img");
+var zoom = document.querySelectorAll(".row-projetos > a");
 
 hover.forEach((element, index) => {
   element.addEventListener("mouseenter", function () {
     bg[index].classList.add("noblur");
-  });
+    zoom[index].classList.add("zoomin");
+});
 
-  element.addEventListener("mouseout", function () {
+element.addEventListener("mouseout", function () {
     bg[index].classList.remove("noblur");
+    zoom[index].classList.remove("zoomin");
   });
 });
