1519a15ef3897c7ecedc5388a5c996d303cdcbdb
Fizene <119943937+bruno-rodrigues0@users.noreply.github.com>
2024-03-09 20:43:03 -0300

    ajustes da identação

--- js/script.js
+++ js/script.js
@@ -2,11 +2,11 @@ var hover = document.querySelectorAll(".hoverlink");
 var bg = document.querySelectorAll(".bg-img");
 
 hover.forEach((element, index) => {
-    element.addEventListener("mouseenter", function () {
-        bg[index].classList.add("noblur");
-    });
+  element.addEventListener("mouseenter", function () {
+    bg[index].classList.add("noblur");
+  });
 
-    element.addEventListener("mouseout", function () {
-        bg[index].classList.remove("noblur");
-    });
+  element.addEventListener("mouseout", function () {
+    bg[index].classList.remove("noblur");
+  });
 });
