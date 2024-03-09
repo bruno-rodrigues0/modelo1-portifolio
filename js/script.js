20c097474afeb711763c7162d3dba26fe047cf44
Fizene <119943937+bruno-rodrigues0@users.noreply.github.com>
2024-03-09 20:39:32 -0300

    project hover finalizado

---
+++ js/script.js
@@ -0,0 +1,12 @@
+var hover = document.querySelectorAll(".hoverlink");
+var bg = document.querySelectorAll(".bg-img");
+
+hover.forEach((element, index) => {
+    element.addEventListener("mouseenter", function () {
+        bg[index].classList.add("noblur");
+    });
+
+    element.addEventListener("mouseout", function () {
+        bg[index].classList.remove("noblur");
+    });
+});
