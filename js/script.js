2d6001bf4846e82060c264ef116c68ee0b49a3ab
Fizene <119943937+bruno-rodrigues0@users.noreply.github.com>
2024-03-09 21:12:23 -0300

    coment forEach function

--- js/script.js
+++ js/script.js
@@ -2,13 +2,20 @@ var hover = document.querySelectorAll(".hoverlink");
 var bg = document.querySelectorAll(".bg-img");
 var zoom = document.querySelectorAll(".row-projetos > a");
 
+// forEach irá chamar uma função entregando ate 3 parametros
+// o primeiro parametro será o elemento atual do array
+// o segundo é o indice desse elemento
+// o terceiro é o array inteiro
+// apenas o primeiro parametro é obrigatório
+// a seguinte função passa por todos os elementos do array hover e adiciona um eventListener, guardando também a informação do seu indice que será utilizado para indicar qual elemento ira sofre a ação da função.
+
 hover.forEach((element, index) => {
   element.addEventListener("mouseenter", function () {
     bg[index].classList.add("noblur");
     zoom[index].classList.add("zoomin");
-});
+  });
 
-element.addEventListener("mouseout", function () {
+  element.addEventListener("mouseout", function () {
     bg[index].classList.remove("noblur");
     zoom[index].classList.remove("zoomin");
   });
